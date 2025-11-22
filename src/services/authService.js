33fdd1eaa39026a2ffae28cc52734bc5fbb62d3e
TiseTiwa <ismailtisetiwa@gmail.com>
2025-11-22 19:11:40 +0100

    added some logics

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,11 +2,14 @@ const BASE_URL = "https://betahouse-backend-pr81.onrender.com/api/auth";
 
 // ---- SIGN UP ----
 export const signupUser = async (formData) => {
-  const response = await fetch(`${BASE_URL}/signup`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData),
-  });
+  const response = await fetch(
+    `https://betahouse-backend-pr81.onrender.com/api/auth/signup`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    }
+  );
 
   const data = await response.json();
 
@@ -19,11 +22,14 @@ export const signupUser = async (formData) => {
 
 // ---- SIGN IN ----
 export const signinUser = async (credentials) => {
-  const response = await fetch(`${BASE_URL}/login`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(credentials),
-  });
+  const response = await fetch(
+    `https://betahouse-backend-pr81.onrender.com/api/auth/login`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(credentials),
+    }
+  );
 
   const data = await response.json();
 
